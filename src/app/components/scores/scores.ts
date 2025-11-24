083cfc2123ceea06dbdd16a842fb36eb9b70f552
bluegglweed <bluegglweed@gmail.com~>
2025-11-23 20:31:07 -0600

    alter edit antes de update

--- src/app/components/scores/scores.ts
+++ src/app/components/scores/scores.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Necesario para ngFor y ngIf
+import { CommonModule } from '@angular/common';
 
 interface ScoreEntry {
   rank: number;
   name: string;
   score: number;
-  time: string; // Tiempo de supervivencia o de juego
+  time: string;
 }
 
 @Component({
@@ -22,7 +22,7 @@ scores: ScoreEntry[] = [
     { rank: 4, name: 'GatoPardo', score: 65432, time: '22:05' },
     { rank: 5, name: 'BugHunter', score: 50111, time: '18:59' },
     { rank: 6, name: 'Player69', score: 45000, time: '17:30' },
-    { rank: 7, name: 'Anon_Dev', score: 33200, time: '15:00' },
+    { rank: 7, name: 'LaVarnilla', score: 33200, time: '15:00' },
   ];
 
   constructor() { }
